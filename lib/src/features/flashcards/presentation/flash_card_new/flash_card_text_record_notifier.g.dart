c318881600d3975dc176e83301d0a9dea10d7e1f
Sofya Noskova <sofya.noskova@gmail.com>
2024-05-20 13:49:39 -0700

    remaining speech recognition files

---
+++ lib/src/features/flashcards/presentation/flash_card_new/flash_card_text_record_notifier.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'flash_card_text_record_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$flashCardTextRecordNotifierHash() =>
+    r'5927f67e370ac624b0c9a576e3a09bedb81a2b5a';
+
+/// See also [FlashCardTextRecordNotifier].
+@ProviderFor(FlashCardTextRecordNotifier)
+final flashCardTextRecordNotifierProvider =
+    AutoDisposeNotifierProvider<FlashCardTextRecordNotifier, String>.internal(
+  FlashCardTextRecordNotifier.new,
+  name: r'flashCardTextRecordNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$flashCardTextRecordNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$FlashCardTextRecordNotifier = AutoDisposeNotifier<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
