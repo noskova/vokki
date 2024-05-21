e6b8c8fe8c98739532f0c9d9e9dec1346b9c1f53
Sofya Noskova <sofya.noskova@gmail.com>
2024-05-20 17:24:43 -0700

    added words speaker

--- lib/src/features/home/presentation/home_screen.dart
+++ lib/src/features/home/presentation/home_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
 import 'package:go_router/go_router.dart';
 import 'package:vokki/src/common_widgets/error_message_widget.dart';
@@ -112,12 +115,147 @@ class CarouselWidget extends StatelessWidget {
   }
 }
 
-class CarouselElementWidget extends StatelessWidget {
+enum TtsState { playing, stopped, paused, continued }
+
+class CarouselElementWidget extends StatefulWidget {
   final FlashCard _flashCard;
-  const CarouselElementWidget({
-    required FlashCard flashCard,
-    super.key,
-  }) : _flashCard = flashCard;
+  const CarouselElementWidget({required FlashCard flashCard, super.key})
+      : _flashCard = flashCard;
+
+  @override
+  State<CarouselElementWidget> createState() => _CarouselElementWidgetState();
+}
+
+class _CarouselElementWidgetState extends State<CarouselElementWidget> {
+  late FlutterTts flutterTts;
+
+  String? language;
+  String? engine;
+  double volume = 0.5;
+  double pitch = 1.0;
+  double rate = 0.5;
+  bool isCurrentLanguageInstalled = false;
+
+  String? _newVoiceText;
+
+  TtsState ttsState = TtsState.stopped;
+
+  bool get isPlaying => ttsState == TtsState.playing;
+  bool get isStopped => ttsState == TtsState.stopped;
+  bool get isPaused => ttsState == TtsState.paused;
+  bool get isContinued => ttsState == TtsState.continued;
+
+  @override
+  initState() {
+    _loadWord(widget._flashCard.word);
+    super.initState();
+    initTts();
+  }
+
+  dynamic initTts() {
+    flutterTts = FlutterTts();
+
+    _setAwaitOptions();
+
+    if (Platform.isAndroid) {
+      _getDefaultEngine();
+      _getDefaultVoice();
+    }
+
+    flutterTts.setStartHandler(() {
+      setState(() {
+        print("Playing");
+        ttsState = TtsState.playing;
+      });
+    });
+
+    flutterTts.setCompletionHandler(() {
+      setState(() {
+        print("Complete");
+        ttsState = TtsState.stopped;
+      });
+    });
+
+    flutterTts.setCancelHandler(() {
+      setState(() {
+        print("Cancel");
+        ttsState = TtsState.stopped;
+      });
+    });
+
+    flutterTts.setPauseHandler(() {
+      setState(() {
+        print("Paused");
+        ttsState = TtsState.paused;
+      });
+    });
+
+    flutterTts.setContinueHandler(() {
+      setState(() {
+        print("Continued");
+        ttsState = TtsState.continued;
+      });
+    });
+
+    flutterTts.setErrorHandler((msg) {
+      setState(() {
+        print("error: $msg");
+        ttsState = TtsState.stopped;
+      });
+    });
+  }
+
+  Future<void> _setAwaitOptions() async {
+    await flutterTts.awaitSpeakCompletion(true);
+  }
+
+  Future<void> _getDefaultEngine() async {
+    var engine = await flutterTts.getDefaultEngine;
+    if (engine != null) {
+      print(engine);
+    }
+  }
+
+  Future<void> _getDefaultVoice() async {
+    var voice = await flutterTts.getDefaultVoice;
+    if (voice != null) {
+      print(voice);
+    }
+  }
+
+  Future<void> _speak() async {
+    await flutterTts.setVolume(volume);
+    await flutterTts.setSpeechRate(rate);
+    await flutterTts.setPitch(pitch);
+
+    if (_newVoiceText != null) {
+      if (_newVoiceText!.isNotEmpty) {
+        await flutterTts.speak(_newVoiceText!);
+      }
+    }
+  }
+
+  Future<void> _stop() async {
+    var result = await flutterTts.stop();
+    if (result == 1) setState(() => ttsState = TtsState.stopped);
+  }
+
+  Future<void> _pause() async {
+    var result = await flutterTts.pause();
+    if (result == 1) setState(() => ttsState = TtsState.paused);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    flutterTts.stop();
+  }
+
+  void _loadWord(String text) {
+    setState(() {
+      _newVoiceText = text;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -134,20 +272,22 @@ class CarouselElementWidget extends StatelessWidget {
             child: Column(
               children: [
                 Text(
-                  _flashCard.word,
+                  widget._flashCard.word,
                   style: Theme.of(context).textTheme.headlineSmall,
                 ),
                 gapH16,
                 IconButton(
                   icon: const Icon(Icons.volume_up_outlined, size: Sizes.p40),
-                  onPressed: () {},
+                  onPressed: () {
+                    _speak();
+                  },
                 ),
                 gapH16,
                 Wrap(
                   children: [
                     PrimaryButton(
                       onPressed: () {},
-                      text: 'Show translation'.hardcoded,
+                      text: 'Translation'.hardcoded,
                     ),
                   ],
                 ),
